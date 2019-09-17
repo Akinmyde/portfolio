deecfd0c46c45f320355bbbd548cf6983a453a11
Akinremi Olumide <akinremiolumide@Akinremis-MacBook-Pro.local>
2019-09-17 19:28:45 +0100

    improve design

--- src/PortFolioCard.jsx
+++ src/PortFolioCard.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const PortFolioCard = ({ imageUrl, title, description, link }) => {
   return (
     <div className="col-md-4 col-sm-6 portfolio-item">
-          <a className="portfolio-link" target="_blank" href={link}>
+          <a className="portfolio-link" href={link}>
             <div className="portfolio-hover">
               <div className="portfolio-hover-content" />
             </div>
             <img className="img-fluid" src={imageUrl} alt="" />
           </a>
           <div className="portfolio-caption">
-            <h4><a href={link} target="_blank">{title}</a></h4>
+            <h4><a href={link} >{title}</a></h4>
             <p className="text-muted">{description}</p>
           </div>
         </div>
